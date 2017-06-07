213716e593ed33eb46e3c68136bad498bb527d32
Densha1053 <a2p2i2t1@icloud.com>
2017-06-07 18:47:37 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -46,8 +46,6 @@ def hello():
         for matcher, action in commands:
             if matcher.search(text):
                 response = action(text)
-                print("tuple")
-                (rc, mid) = mqttc.publish("/Benz1053/room2",text, qos = 2)
                 break
         else:
             response = 'コマネチ'
@@ -56,42 +54,6 @@ def hello():
 
     return ''
 
-import context
-import paho.mqtt.client as mqtt
-
-
-def on_connect(mqttc, obj, flags, rc):
-    print("rc: " + str(rc))
-
-
-def on_message(mqttc, obj, msg):
-    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-
-
-def on_publish(mqttc, obj, mid):
-    print("mid: " + str(mid))
-
-
-def on_subscribe(mqttc, obj, mid, granted_qos):
-    print("Subscribed: " + str(mid) + " " + str(granted_qos))
-
-
-def on_log(mqttc, obj, level, string):
-    print(string)
-
-
-mqttc = mqtt.Client()
-mqttc.on_message = on_message
-mqttc.on_connect = on_connect
-mqttc.on_publish = on_publish
-mqttc.on_subscribe = on_subscribe
-# Uncomment to enable debug messages
-# mqttc.on_log = on_log
-mqttc.username_pw_set("Benz1053","benz1053")
-mqttc.connect("km1.io", 1883, 60)
-mqttc.subscribe("/Benz1053/room1", 0)
-
-mqttc.loop_forever()
 
 if __name__ == "__main__":
     context = ('cert/server.pem', 'cert/privkey.pem')
