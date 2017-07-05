673b6d2a8b669210f86cd47b89eb24a75aa0dc83
Densha1053 <a2p2i2t1@icloud.com>
2017-07-05 19:26:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_URL, 
-    'https://ami1053.herokuapp.com/action_page.php?1=Mickey&lastname=Mouse'
+    'https://ami1053.herokuapp.com/action_page.php'
 );
 $content = curl_exec($ch);
 echo $content;
